65d1b8f4cf62a4a69cc558e1704e19eb28c1fef1
Tiity <cadet_a@etna-alternance.net>
2022-08-30 17:14:09 +0200

    Startup distributeur TP

---
+++ src/main/java/fr/m2i/javaspringmvc/model/Product.java
@@ -0,0 +1,26 @@
+package fr.m2i.javaspringmvc.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    
+}
---
+++ src/main/webapp/WEB-INF/view/distributeur.jsp
@@ -0,0 +1,12 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Distributeur Spring MVC</title>
+    </head>
+    <body>
+        <h1>Crédit : ${balance}</h1>
+    </body>
+</html>
